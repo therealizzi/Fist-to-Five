0f1d2c9787e8327188638c8053e65225f71ab9c0
stabrr <bobbystabile@yahoo.com>
2018-01-10 19:03:33 -0600

    The greatest commit in the world

--- controllers/new_controller.js
+++ controllers/new_controller.js
@@ -13,9 +13,9 @@ router.get("/", function(req, res) {
         order: 'ID DESC',
         limit: 5
     }).then(function(dbFist5) {
-        console.log(JSON.stringify(dbFist5[0].dataValues));
+        // console.log(JSON.stringify(dbFist5[0].dataValues));
         var hbsObject = { fist5: dbFist5 };
-        console.log("what " + hbsObject);
+        // console.log("what " + hbsObject);
         return res.render("index", hbsObject);
         // res.render(dbFist5)
 
@@ -41,14 +41,65 @@ router.get("/history", function(req, res) {
 
 // POST ROUTES
 router.post("/", function(req, res) {
-    var count = req.body.count;
-    db.Fist5.findById(1).then(dbFist5 => {
+    var fistSplit = req.body.count;
+    fistSplit = (fistSplit).split(".");
+    fistSelected = fistSplit[1];
+    console.log(fistSplit);
+    var topicID = parseInt(fistSplit[0]);
+    var fist1 = 0;
+    var fist2 = 0;
+    var fist3 = 0;
+    var fist4 = 0;
+    var fist5 = 0;
+    switch (fistSelected) {
+        case "f1":
+            var fistNum = 1;
+            fist1 = 1;
+            break;
+        case "f2":
+            var fistNum = 2;
+            fist2 = 1;
+            break;
+        case "f3":
+            var fistNum = 3;
+            fist3 = 1;
+            break;
+        case "f4":
+            var fistNum = 4;
+            fist4 = 1;
+            break;
+        case "f5":
+            var fistNum = 5;
+            fist5 = 1;
+            break;
+        default:
+            var fistNum = 0;
+
+    }
+    // console.log("request body" + req.body.count);
+    db.Fist5.findById(topicID).then(dbFist5 => {
+        console.log("Help ME!");
+        var total = (JSON.stringify(dbFist5.ftotal));
+        var count = (JSON.stringify(dbFist5.count));
+        var average = total / count;
+        console.log("Total = " + total);
+        console.log("count = " + count);
+        console.log("average = " + average);
+        (dbFist5).update({
+            'favg': average
+        })
         return (dbFist5).increment({
             'count': 1,
-            count: 1
+            'f1': fist1,
+            'f2': fist2,
+            'f3': fist3,
+            'f4': fist4,
+            'f5': fist5,
+            'ftotal': fistNum
         })
+
     });
-    console.log(count);
+    // console.log(count);
     //needs to post updates to F1, F2, F3, F4, F5
     res.redirect("/")
 })
